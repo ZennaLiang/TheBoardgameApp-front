561d2e98d06261a017ebae9699c79cbaf43ca777
Louie Tse <28274608+tselouie@users.noreply.github.com>
2020-12-10 15:17:18 -0500

    gettingstarted toast

---
+++ src/components/GettingStartedAlert.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth";
+import { getUser } from "../user/apiUser";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2)
+    }
+  }
+}));
+
+export default function GettingstartedAlert() {
+  const classes = useStyles();
+
+  const [hasSynced, setSync] = useState(false);
+  if (isAuthenticated()) {
+    getUser(isAuthenticated().user._id, isAuthenticated().token).then(
+      person => {
+        console.log(person.bggUsername);
+        if (person.bggUsername) {
+          setSync(true);
+        } else {
+          setSync(false);
+        }
+      }
+    );
+
+    return (
+      <div className={classes.root}>
+        {!hasSynced ? (
+          <Alert severity="info" color="info">
+            <Link
+              to={"/user/edit/bgg/" + isAuthenticated().user._id}
+              className="primary"
+            >
+              Get Started
+            </Link>{" "}
+            by syncing your BoardGameGeeks Account.
+          </Alert>
+        ) : null}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
