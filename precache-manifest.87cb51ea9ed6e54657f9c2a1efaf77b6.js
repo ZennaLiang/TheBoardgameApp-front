4d7f3bc9cbe7d88aa439b38da9f190816ef58ad5
Louie Tse <tselouie@gmail.com>
2022-12-22 14:27:59 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.87cb51ea9ed6e54657f9c2a1efaf77b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6357ca35fbab1263a3050dcfa6ec8ea8",
+    "url": "/BoardgameGuru/index.html"
+  },
+  {
+    "revision": "68000de326e732f227de",
+    "url": "/BoardgameGuru/static/css/2.664f5629.chunk.css"
+  },
+  {
+    "revision": "1a86c3c89a681a307dec",
+    "url": "/BoardgameGuru/static/css/main.69429b8e.chunk.css"
+  },
+  {
+    "revision": "68000de326e732f227de",
+    "url": "/BoardgameGuru/static/js/2.4752edbe.chunk.js"
+  },
+  {
+    "revision": "c11b1bb0f8c9c6cd71e15205a1de2580",
+    "url": "/BoardgameGuru/static/js/2.4752edbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a86c3c89a681a307dec",
+    "url": "/BoardgameGuru/static/js/main.7584da12.chunk.js"
+  },
+  {
+    "revision": "ea0e46b0968ad5fec920",
+    "url": "/BoardgameGuru/static/js/runtime-main.8926dc09.js"
+  },
+  {
+    "revision": "69d79931b0083a75f9a809237fcefe7b",
+    "url": "/BoardgameGuru/static/media/defaultPostImg.69d79931.jpg"
+  },
+  {
+    "revision": "a25c147470c59d7ee7b8641209baad5b",
+    "url": "/BoardgameGuru/static/media/noImageAvailable.a25c1474.jpg"
+  }
+]);
